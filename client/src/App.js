086759722e66aaeaac7a648b9c61f7b6d98a0630
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2022-09-01 13:48:03 -0400

    implementing complements and desing

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,25 @@ import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
 import Home from "./pages/Home"
 import Page2 from './pages/Page2';
 import Page3 from './pages/Page3';
+import SignIn from './pages/SignIn';
+import Login from './pages/Login';
+//Components
+import Footer from "./components/Footer"
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/page2" element={<Page2 />}/>
           <Route path="/page3" element={<Page3 />}/>
+          <Route path="/Login" element={<Login/>}/>
+          <Route path="/SignIn" element={<SignIn/>}/>
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
---
+++ client/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Footer.css"
+
+export default function Footer() {
+  return(
+    <div className="footer">
+        <div>
+
+        </div>
+        <p> &copy; 2022 All rigths reserved</p>
+    </div>
+  )
+}
+
+
