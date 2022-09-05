595abc12738308bf96d5ed9bec99ff577ee87483
joe_bob_21@hotmail.com <crbailey@isd535.org>
2022-09-05 13:53:03 -0500

    refresh issues

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -12,3 +12,13 @@ export function logout() {
     localStorage.removeItem('token')
     window.location = '/'
 }
+
+// export function isAuthenticated() {
+//     const token = localStorage.getItem('token');
+  
+//     if (!token) return false;
+  
+//     const decoded = decode(token);
+  
+//     if (decoded.exp > Date.now() / 1000) return decoded.data;
+//   }
