4bb70445b5d1d9d725dc5d49fe320e7b8ebeeb63
joe_bob_21@hotmail.com <crbailey@isd535.org>
2022-09-06 11:49:34 -0500

    stat changes, does not work

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,9 +9,9 @@ export default function Footer() {
   return(
     <div className="footer">
         <div className="socialMedia">
-          <a href=""><BsEnvelope /></a> 
+          <a href="#"><BsEnvelope /></a> 
           <a href="https://github.com/cr7st74n/Purpel-Cobra-Finances"><AiFillGithub /></a>
-          <a href=""><AiFillLinkedin /></a>
+          <a href="#"><AiFillLinkedin /></a>
       
         </div>
         <p> &copy; 2022 All rigths reserved</p>
--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import {React} from 'react'
+import {gql, useQuery} from '@apollo/client'
+
+const GET_EXPENSES = gql`
+query Query {
+  getExpenses {
+    name
+    expenseType
+    price
+  }
+}
+`;
 
 export default function Stats(){
+  const [error, loading, data] = useQuery(GET_EXPENSES)
+    console.log(data.getExpenses);
+  
     return (
       <div>
           <h2>Expenses Breakdown</h2>
+          <div className='Auto'>
+            {error && <p className="error">{error.message}</p>}
+            {loading && <p>Loading...</p>}
+            {data && (
+            <ul>
+            {data.getExpenses.map(exp => (
+              <li key={exp._id}>{exp.name} - {exp.price}</li>
+            ))}
+            </ul>
+            )}
+          </div>
       </div>
     )
   }
